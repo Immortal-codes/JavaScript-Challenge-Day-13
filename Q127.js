c0e1e57653691bc0f8d1fd52e0fafb22e16f1158
Immortal-codes <rajanraj8979@gmail.com>
2022-02-12 16:26:44 +0530

    first commit

---
+++ Q127.js
@@ -0,0 +1,10 @@
+//Write a JavaScript program to reverse the order of the bits in a given integer
+
+
+function mirror_bits(n) {
+    return parseInt(n.toString(2).split("").reverse().join(""), 2);
+  }
+  
+  console.log(mirror_bits(56));
+  console.log(mirror_bits(234));
+  
